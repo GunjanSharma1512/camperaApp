74c9a6a890f816637997725ce075a3dd01e95ca3
arti97 <arathi.n97@gmail.com>
2018-03-16 09:46:15 +0530

    Integrated

--- app/src/main/java/com/example/gunjan/camperaapp/MainActivity.java
+++ app/src/main/java/com/example/gunjan/camperaapp/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.gunjan.camperaapp;
 
+import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Handler;
+import android.os.Message;
 import android.provider.MediaStore;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Environment;
 import java.io.File;
+
+import android.util.Log;
 import android.widget.*;
 import android.view.View;
 import android.net.Uri;
@@ -18,11 +29,14 @@ import android.Manifest;
 import android.support.v4.app.ActivityCompat;
 
 import java.io.IOException;
+import java.util.Calendar;
 import java.util.Date;
 import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.Locale;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener {
     private Button takePictureButton;
     private ImageView imageView;
     Uri file;
@@ -30,6 +44,14 @@ public class MainActivity extends AppCompatActivity {
     String mCurrentPhotoPath;
     File photoFile;
     Uri photoURI;
+    private TextView latitudePosition;
+    private TextView longitudePosition;
+    private TextView currentCity;
+    private LocationManager locationManager;
+    private Location location;
+    private final int REQUEST_LOCATION = 200;
+    int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +66,112 @@ public class MainActivity extends AppCompatActivity {
             takePictureButton.setEnabled(false);
             ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE }, 0);
         }
+
+        latitudePosition = (TextView) findViewById(R.id.latitude);
+        longitudePosition = (TextView) findViewById(R.id.longitude);
+        currentCity = (TextView) findViewById(R.id.city);
+        locationManager = (LocationManager) getSystemService(Service.LOCATION_SERVICE);
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_LOCATION);
+            Toast.makeText(MainActivity.this, "permission not given", Toast.LENGTH_SHORT).show();
+        } else {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 100, 2, MainActivity.this);
+            if (locationManager != null) {
+                location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            }
+        }
+        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            if (location != null) {
+                latitudePosition.setText(String.valueOf(location.getLatitude()));
+                longitudePosition.setText(String.valueOf(location.getLongitude()));
+                Log.d("AAAAAAAAAAAAAA", String.valueOf(location.getLatitude()));
+                Log.d("AAAAAAAAAAAAAA", String.valueOf(location.getLongitude()));
+                getAddressFromLocation(location, getApplicationContext(), new GeoCoderHandler());
+            }
+        } else {
+            // showGPSDisabledAlertToUser();
+            Toast.makeText(MainActivity.this, "provider is disabled", Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        if (provider.equals(LocationManager.GPS_PROVIDER)) {
+            Toast.makeText(MainActivity.this, "provider is disabled", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+    }
+
+    public static void getAddressFromLocation(final Location location, final Context context, final Handler handler) {
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                Geocoder geocoder = new Geocoder(context, Locale.getDefault());
+                String result = null;
+                try {
+                    List<Address> list = geocoder.getFromLocation(
+                            location.getLatitude(), location.getLongitude(), 1);
+                    if (list != null && list.size() > 0) {
+                        Address address = list.get(0);
+                        // sending back first address line and locality
+                        result = address.getAddressLine(0) + ", " + address.getLocality() + ", " + address.getCountryName();
+                    }
+                } catch (IOException e) {
+                    Log.e(TAG, "Impossible to connect to Geocoder", e);
+                } finally {
+                    Message msg = Message.obtain();
+                    msg.setTarget(handler);
+                    if (result != null) {
+                        msg.what = 1;
+                        Bundle bundle = new Bundle();
+                        bundle.putString("address", result);
+                        msg.setData(bundle);
+                    } else
+                        msg.what = 0;
+                    msg.sendToTarget();
+                }
+            }
+        };
+        thread.start();
+    }
+
+    private class GeoCoderHandler extends Handler {
+        @Override
+        public void handleMessage(Message message) {
+            String result;
+            switch (message.what) {
+                case 1:
+                    Bundle bundle = message.getData();
+                    result = bundle.getString("address");
+                    break;
+                default:
+                    result = null;
+            }
+            // currentCity.setText(result);
+            Calendar calendar = Calendar.getInstance();
+            // calendar.setTime(yourdate);
+            int hours = calendar.get(Calendar.HOUR_OF_DAY);
+            int day = calendar.get(Calendar.DAY_OF_MONTH);
+            int month = calendar.get(Calendar.MONTH);
+            int year = calendar.get(Calendar.YEAR);
+
+            currentCity.setText("TIME: " + hours + " DATE: " + day + "/" + month + "/" + year);
+
+
+        }
     }
 
     @Override
@@ -53,6 +181,27 @@ public class MainActivity extends AppCompatActivity {
                     && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
                 takePictureButton.setEnabled(true);
             }
+        } else if (requestCode==99){
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                // permission was granted, yay! Do the
+                // location-related task you need to do.
+                if (ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.ACCESS_FINE_LOCATION)
+                        == PackageManager.PERMISSION_GRANTED) {
+
+                    //Request location updates:
+                    locationManager.requestLocationUpdates(locationManager.GPS_PROVIDER, 400, 1, this);
+                }
+
+            } else {
+
+                // permission denied, boo! Disable the
+                // functionality that depends on this permission.
+
+            }
+            return;
         }
     }
 
@@ -128,5 +277,6 @@ public class MainActivity extends AppCompatActivity {
         box.setText(mCurrentPhotoPath);
         return image;
     }
+
 
 }
