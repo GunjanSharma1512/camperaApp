64fa888cce709e2d6ef4ef9013a76016d92d563f
aditi2205 <aditi22aggarwal@gmail.com>
2018-03-26 13:10:25 +0530

    Added volley

--- app/src/main/java/com/example/gunjan/camperaapp/ShowActivity.java
+++ app/src/main/java/com/example/gunjan/camperaapp/ShowActivity.java
@@ -16,9 +16,20 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.w3c.dom.Text;
 
@@ -27,12 +38,16 @@ import java.io.FileDescriptor;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ShowActivity extends AppCompatActivity {
 
     ImageView imageView;
     TextView Hash, Encryp;
     DatabaseHelper databaseHelper;
+    Button send;
+    Bitmap bm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +55,7 @@ public class ShowActivity extends AppCompatActivity {
         setContentView(R.layout.activity_show);
         imageView =  (ImageView) findViewById(R.id.image);
         Hash = (TextView) findViewById(R.id.hash);
+        send = (Button) findViewById(R.id.send);
         Encryp = (TextView) findViewById(R.id.encryp);
         databaseHelper =  new DatabaseHelper(this);
 
@@ -55,6 +71,62 @@ public class ShowActivity extends AppCompatActivity {
         intent.setType("image/*");
         intent.setAction(Intent.ACTION_GET_CONTENT);
         startActivityForResult(Intent.createChooser(intent, "Select File"),1);
+
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                bm.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+                byte[] imageBytes = baos.toByteArray();
+                final String imageString = Base64.encodeToString(imageBytes, Base64.DEFAULT);
+                RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+                String url ="http://172.16.75.172:8000/unhash/";
+                StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>(){
+                    @Override
+                    public void onResponse(String s) {
+                        if(s.equals("true")){
+                            Toast.makeText(ShowActivity.this, "Uploaded Successful", Toast.LENGTH_LONG).show();
+                        }
+                        else{
+                            Toast.makeText(ShowActivity.this, "Not same"+s, Toast.LENGTH_LONG).show();
+                        }
+                    }
+                },new Response.ErrorListener(){
+                    @Override
+                    public void onErrorResponse(VolleyError volleyError) {
+                        Toast.makeText(ShowActivity.this, "Some error occurred -> "+volleyError, Toast.LENGTH_LONG).show();;
+                    }
+                }) {
+                    //adding parameters to send
+                    @Override
+                    protected Map<String, String> getParams() throws AuthFailureError {
+                        Map<String, String> parameters = new HashMap<String, String>();
+                        parameters.put("photo", imageString);
+                        parameters.put("hashcode", Hash.getText().toString());
+                        parameters.put("encrypted", Encryp.getText().toString());
+                        return parameters;
+                    }
+                };
+                queue.add(request);
+                /*request.setRetryPolicy(new RetryPolicy() {
+                    @Override
+                    public int getCurrentTimeout() {
+                        return 0;
+                    }
+
+                    @Override
+                    public int getCurrentRetryCount() {
+                        return 0;
+                    }
+
+                    @Override
+                    public void retry(VolleyError error) throws VolleyError {
+                        Toast.makeText(ShowActivity.this, "blabla", Toast.LENGTH_SHORT).show();
+                    }
+                });*/
+                Toast.makeText(ShowActivity.this, "DONEEEEEE", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
@@ -94,7 +166,7 @@ public class ShowActivity extends AppCompatActivity {
                     Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
                 }
                 databaseHelper.insertImage(photoURI.toString(),hashed, decrypted.getText().toString());*/
-                Bitmap bm=null;
+                bm=null;
                 if (data != null) {
                     try {
                         bm = MediaStore.Images.Media.getBitmap(getApplicationContext().getContentResolver(), data.getData());
@@ -108,8 +180,8 @@ public class ShowActivity extends AppCompatActivity {
                 imageView.setMaxHeight(200);
                 String path = data.toURI().toString();
                 ImageHelper imageHelper = databaseHelper.getImage(path);
-                Hash.append(imageHelper.getHashcode().toString());
-                Encryp.append(imageHelper.getEncrypted().toString());
+                Hash.setText(imageHelper.getHashcode().toString());
+                Encryp.setText(imageHelper.getEncrypted().toString());
             }
         }
 
