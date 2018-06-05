0c53732d071745b03ac92ad65b365b4a65acebcd
Akshama <akshama96@gmail.com>
2018-06-05 08:45:58 +0530

    Notification for Internet Availability added

--- app/src/main/java/com/example/gunjan/camperaapp/NetworkChangeReceiver.java
+++ app/src/main/java/com/example/gunjan/camperaapp/NetworkChangeReceiver.java
@@ -1,5 +1,7 @@
 package com.example.gunjan.camperaapp;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -7,6 +9,7 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.CountDownTimer;
 import android.provider.ContactsContract;
+import android.support.v7.app.NotificationCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
@@ -23,7 +26,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Created by Arti on 3/31/2018.
+ * Created by Akshama PC on 2/06/2018.
  */
 
 public class NetworkChangeReceiver extends BroadcastReceiver{
@@ -50,6 +53,7 @@ public class NetworkChangeReceiver extends BroadcastReceiver{
                             Log.v(LOG_TAG, "Now you are connected to Internet!");
                             Toast.makeText(context, "Internet available via Broadcast receiver", Toast.LENGTH_SHORT).show();
                             isConnected = true;
+                            Notification(context,"Internet available, Click HERE to upload your images");
                             /*autoUpload(context);
                             Toast.makeText(context, "Upload function called", Toast.LENGTH_SHORT).show();*/
                             // do your processing here ---
@@ -65,10 +69,25 @@ public class NetworkChangeReceiver extends BroadcastReceiver{
         Log.v(LOG_TAG, "You are not connected to Internet!");
         Toast.makeText(context, "Internet NOT available via Broadcast receiver", Toast.LENGTH_SHORT).show();
         isConnected = false;
+        Notification(context,"Internet NOT available");
         return false;
     }
 
+    public void Notification(Context context, String message) {
+        NotificationCompat.Builder builder = (NotificationCompat.Builder) new NotificationCompat.Builder(context)
+                .setSmallIcon(R.mipmap.ic_launcher) //icon
+                .setContentTitle("DAVPConnect") //tittle
+                .setAutoCancel(true)//swipe for delete
+                .setContentText(message); //content
+        Intent notificationIntent = new Intent(context,Login_Registration.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(context, 0, notificationIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(contentIntent);
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 
+        notificationManager.notify(1, builder.build()
+        );
+    }
 
 
 }
